aa48976c294143792d28abe5cf599bb754a4704a
robb <robert.patryk.grzelak@gmail.com>
2020-10-22 13:45:46 +0200

    Update nn4.py

--- nn4.py
+++ nn4.py
@@ -9,22 +9,29 @@ X = [[1, 2, 3, 2.5],
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 layer3 = Layer_Dense(2, 3)
+out = Layer_Dense(3, 1)
 
 layer1.forward(X)
+print(f'5 perceptrons')
 print(layer1.output)
 layer2.forward(layer1.output)
+print(f'2 perceptrons')
 print(layer2.output)
 layer3.forward(layer2.output)
+print(f'3 perceptrons')
 print(layer3.output)
+out.forward(layer3.output)
+print(f'1 perceptron')
+print(out.output)
 
 
 
