0c72cdf822cf382b3d4cab2d9fd8a3fd989324c9
robb <robert.patryk.grzelak@gmail.com>
2021-01-05 22:28:00 +0100

    Update Full code p344.py

--- NNFS/Full code p344.py
+++ NNFS/Full code p344.py
@@ -392,32 +392,65 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs = self.dinputs / samples
 
 
+# Dropout
+class Layer_Dropout:
+    # Init
+    def __init__(self, rate):
+        # Store rate, we invert it as for example for dropout
+        # of 0.1 we need success rate of 0.9
+        self.rate = 1 - rate
+    # Forward pass
+    def forward(self, inputs):
+        # Save input values
+        self.inputs = inputs
+        # Generate and save scaled mask
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+        # Apply mask to output values
+        self.output = inputs * self.binary_mask
+    # Backward pass
+    def backward(self, dvalues):
+        # Gradient on values
+        self.dinputs = dvalues * self.binary_mask
+        # Let’s take this new dropout layer, and add it between our two dense layers. First defining it:
+        # Create Dense layer with 2 input features and 64 output values
+        dense1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
+        # Create ReLU activation (to be used with Dense layer):
+        activation1 = Activation_ReLU()
+        # Create dropout layer
+        dropout1 = Layer_Dropout(0.1)
+
+
+
+
+
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 # Create Dense layer with 2 input features and 64 output values
-dense1 = Layer_Dense(2, 600, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
+dense1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
 # Create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
+# Create dropout layer
+dropout1 = Layer_Dropout(0.1)
 # Create second Dense layer with 64 input features (as we take output
 # of previous layer here) and 3 output values (output values)
-dense2 = Layer_Dense(600, 3)
+dense2 = Layer_Dense(64, 3)
 # Create Softmax classifier's combined loss and activation
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 # Create optimizer
-optimizer = Optimizer_Adam(learning_rate=0.02, decay=5e-7)
+optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-5)
 # Train in loop
 for epoch in range(10001):
 
     # Perform a forward pass of our training data through this layer
     dense1.forward(X)
-
     # Perform a forward pass through activation function
     # takes the output of first dense layer here
     activation1.forward(dense1.output)
-
+    # Perform a forward pass through Dropout layer
+    dropout1.forward(activation1.output)
     # Perform a forward pass through second Dense layer
     # takes outputs of activation function of first layer as inputs
-    dense2.forward(activation1.output)
+    dense2.forward(dropout1.output)
     # Perform a forward pass through the activation/loss function
     # takes the output of second dense layer here and returns loss
     data_loss = loss_activation.forward(dense2.output, y)
@@ -443,7 +476,8 @@ for epoch in range(10001):
     # Backward pass
     loss_activation.backward(loss_activation.output, y)
     dense2.backward(loss_activation.dinputs)
-    activation1.backward(dense2.dinputs)
+    dropout1.backward(dense2.dinputs)
+    activation1.backward(dropout1.dinputs)
     dense1.backward(activation1.dinputs)
     # Update weights and biases
     optimizer.pre_update_params()
@@ -451,9 +485,10 @@ for epoch in range(10001):
     optimizer.update_params(dense2)
     optimizer.post_update_params()
 
+
 # Validate the model
 # Create test dataset
-X_test, y_test = spiral_data(samples=1000, classes=3)
+X_test, y_test = spiral_data(samples=100, classes=3)
 # Perform a forward pass of our testing data through this layer
 dense1.forward(X_test)
 # Perform a forward pass through activation function
