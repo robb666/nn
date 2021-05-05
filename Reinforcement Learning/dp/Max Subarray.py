1f9f05b085c002d4fe2c207a9cb9b794b64703cf
robb666 <robert.patryk.grzelak@gmail.com>
2021-05-05 22:55:18 +0200

    Update Max Subarray.py

--- Reinforcement Learning/dp/Max Subarray.py
+++ Reinforcement Learning/dp/Max Subarray.py
@@ -1,10 +1,11 @@
+# importing the required module
+import timeit
 
 
 
 
 
-
-
+setup = '''
 def maxContSubarr(arr):
 
     for _ in range(len(arr)):
@@ -20,52 +21,63 @@ def maxContSubarr(arr):
 
 with open('test_maxsubarr.txt') as arr:
     arr = arr.read()[9:]
-    print(maxContSubarr(arr))
+    maxContSubarr(arr)
+'''
+
+times = timeit.repeat(setup = setup,
+                          repeat = 100,
+                          number = 10000)
+print(sum(times))
 
 
 
-
+setup2 = '''
 def maxSubarr(arr):
 
     arr2 = []
-    for i in arr:
-        if i > 0:
-            arr2.append(i)
-    if sum(arr2) > 0:
-        max_subsequence = sum(arr2)
-    else:
-        max_subsequence = max(arr)
+    for _ in range(len(arr)):
+        for i in arr:
+            if i > 0:
+                arr2.append(i)
+        if sum(arr2) > 0:
+            max_subsequence = sum(arr2)
+        else:
+            max_subsequence = max(arr)
 
-    dp_start = []
-    s1 = 0
-    for i in list(reversed(arr)):
-        s1 += i
-        mx1 = max(i, s1)
-        dp_start.append(mx1)
+        dp_start = []
+        s1 = 0
+        for i in list(reversed(arr)):
+            s1 += i
+            mx1 = max(i, s1)
+            dp_start.append(mx1)
 
-    dp_stop = []
-    s2 = 0
-    for j in arr:
-        s2 += j
-        mx2 = max(j, s2)
-        dp_stop.append(mx2)
+        dp_stop = []
+        s2 = 0
+        for j in arr:
+            s2 += j
+            mx2 = max(j, s2)
+            dp_stop.append(mx2)
 
-    start = (len(arr) - 1) - dp_start.index(max(dp_start))
-    stop = dp_stop.index(max(dp_stop)) + 1
-    max_subarray = sum(arr[start:stop])
+        start = (len(arr) - 1) - dp_start.index(max(dp_start))
+        stop = dp_stop.index(max(dp_stop)) + 1
+        max_subarray = sum(arr[start:stop])
 
-    return max_subarray, max_subsequence
+        return max_subarray, max_subsequence
 
 
 with open('test_maxsubarr.txt') as arr:
     arr = list(map(int, arr.read()[9:].split()))
 
-    print(maxSubarr(arr))
+    maxSubarr(arr)
+'''
 
 
 
 
-
+times = timeit.repeat(setup = setup2,
+                          repeat = 100,
+                          number = 10000)
+print(sum(times))
 
 
 
