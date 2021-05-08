1de46cea64f484c2ea49be3bd88dc5c82618ff8f
robb666 <56496611+robb666@users.noreply.github.com>
2021-05-08 03:20:32 +0200

    Update Maximum Sum Increasing Subsequence.py

--- Reinforcement Learning/dp/Maximum Sum Increasing Subsequence.py
+++ Reinforcement Learning/dp/Maximum Sum Increasing Subsequence.py
@@ -1,20 +1,29 @@
 
 
 def MaximumSumIncreasingSubsequence(arr):
-    max_sub = arr
-    ind_arr = []
-    # j = 0
+    max_sub = arr.copy()
+    ind_arr = [0, 1, 2, 3, 4, 5, 6]
 
     for i in range(1, len(arr)):
         for j in range(len(arr)):
             if arr[j] < arr[i] and j < i:
-                max_sub[i] = max(max_sub[i], max_sub[j] + arr[i])
+                max_sub[i] = max(max_sub[j] + arr[i], max_sub[i])
+                ind_arr[i] = max_sub.index(max_sub[j])
 
-    return max_sub
+    return arr, max_sub, ind_arr
 
 
 arr = [4, 6, 1, 3, 8, 4, 6]
 print(MaximumSumIncreasingSubsequence(arr))
+
+
+
+
+
+
+
+
+
 
 
 
