92389dc3ed66f3a3bafa12d88c6690ded57c57b9
robb666 <robert.patryk.grzelak@gmail.com>
2021-05-11 19:52:55 +0200

    Update Maximum Sum Increasing Subsequence.py

--- Reinforcement Learning/dp/Maximum Sum Increasing Subsequence.py
+++ Reinforcement Learning/dp/Maximum Sum Increasing Subsequence.py
@@ -1,4 +1,6 @@
 
+import sys
+
 
 def MaximumSumIncreasingSubsequence(arr):
     max_sub = arr.copy()
@@ -20,29 +22,99 @@ def MaximumSumIncreasingSubsequence(arr):
     return max(max_sub), list(reversed(partial_values))
 
 
-
-arr = [4, 6, 1, 3, 8, 4, 6]
-# arr = [1, 101, 2, 3, 100, 4, 5]
+# arr = [4, 6, 1, 3, 8, 4, 6]
+arr = [1, 101, 2, 3, 100, 4, 5]
+# arr = [3, 2, 6, 4, 5, 1]
+# arr = [10, 5, 4, 3]
+# arr = [3, 4, 5, 10]
+# arr = [3, 2, 6, 4, 5, 1]
 
 print(MaximumSumIncreasingSubsequence(arr))
+print()
+
+###################################################
+def constructMaxSumIS(arr, n):
+    L = []
+    index = 0
+    for i in arr:
+        L.append([i, index])
+        index += 1
+
+    L[0][1] = -1
+
+    for i in range(1, n):  # można bez 1
+        for j in range(i):
+            if arr[i] > arr[j] and L[i][0] < arr[i] + L[j][0]:
+                L[i][0] = arr[i] + L[j][0]
+                L[i][1] = j
+    print(L)
+    maxi, currIndex, track = -sys.maxsize, 0, 0
+
+    for p in L:
+        if p[0] > maxi:
+            maxi = p[0]
+            currIndex = track
+
+        track += 1
+
+    result = []
+
+    while currIndex >= 0:
+        result.append(arr[currIndex])
+        previousIndex = L[currIndex][1]
+
+        if currIndex == previousIndex:
+            break
+
+        currIndex = previousIndex
+
+    for i in range(len(result) -1, -1, -1):
+        print(result[i], end=' ')
 
 
+array = [1, 101, 2, 3, 100, 4, 5 ]
+# array = [3, 2, 6, 4, 5, 1]
+n = len(array)
 
 
+constructMaxSumIS(array, len(array))
+print()
+print()
 
 
+############################################
+def findSum(arr):
 
-# ind_arr = []
-# arr = [4, 6, 1, 3, 8, 4, 6]
-# max_sub = [4, 10, 1, 4, 18, 8, 14]
-# for i in range(1, len(max_sub)):
-#     # for j in range(len(max_sub)):
-#         if max_sub[i-1] < max_sub[i]:
-#
-#             ind_arr.append(max_sub.index(max_sub[i-1]))
-#
-#
-# print(ind_arr)
+    summ = 0
+    for _ in arr:
+        summ += 1
+    return summ
 
 
+def printMaxSumIS(arr, n):
+    L = [[] for _ in range(n)]
+    L[0].append(arr[0])
 
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] > arr[j] and findSum(L[i]) < findSum(L[j]):
+                for e in L[j]:
+                    if e not in L[i]:
+                        L[i].append(e)
+        L[i].append(arr[i])
+
+    res = L[0]
+    for x in L:
+        if findSum(x) > findSum(res):
+            res = x
+
+    for i in res:
+        print(i, end=' ')
+
+    return L
+
+
+arr = [3, 2, 6, 4, 5, 1]
+
+n = findSum(arr)
+print(printMaxSumIS(arr, n))
