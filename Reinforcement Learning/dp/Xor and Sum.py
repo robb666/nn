5533eb686b3ae66e337462e83cfaec5031927934
robb666 <56496611+robb666@users.noreply.github.com>
2021-05-14 09:51:05 +0200

    Update Xor and Sum.py

--- Reinforcement Learning/dp/Xor and Sum.py
+++ Reinforcement Learning/dp/Xor and Sum.py
@@ -1,19 +1,17 @@
-
+from operator import xor
 
 
 def xorAndSum(a, b):
     # Write your code here
 
     sum_modulo = 10 ** 9 + 7
-    # to_bin = format(a, 'b')
-    bina = int(str(a), 2)
-    binb = int(str(b), 2)
 
-    # sum_ab = 0
-    # for i in range(10):
-    sum_ab = 5 % 5
 
-    return sum_modulo, bina, binb, sum_ab
+    sum_ab = 0
+    for i in range(314159):
+        sum_ab = xor(a, b << i)
+        if sum_ab == sum_modulo:
+            return sum_modulo, sum_ab
 
 
 a = 10
