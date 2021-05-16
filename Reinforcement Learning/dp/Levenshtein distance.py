2e192057b0225895470015be119da221afaa3742
robb666 <56496611+robb666@users.noreply.github.com>
2021-05-17 01:16:18 +0200

    Update Levenshtein distance.py

--- Reinforcement Learning/dp/Levenshtein distance.py
+++ Reinforcement Learning/dp/Levenshtein distance.py
@@ -3,11 +3,13 @@ import numpy as np
 
 def L_matrix(str1, str2):
     matrix = [[0 for _ in range(len(str1) + 2)] for _ in range(len(str2) + 2)]
-    for i in range(len(str1)):
-        matrix[0][i + 2] = str1[i]
+    for row in range(len(str1)):
+        matrix[0][row + 2] = str1[row]
+        matrix[1][row + 1] = row
 
-    for j in range(len(str2)):
-        matrix[j + 2][0] = str2[j]
+    for column in range(len(str2)):
+        matrix[column + 2][0] = str2[column]
+        matrix[column + 1][1] = column
 
     return np.array(matrix)
 
@@ -16,17 +18,18 @@ def L_matrix(str1, str2):
 
 def L_dist(str1, str2):
     matrix = L_matrix(str1, str2)
-    for i in range(2, len(str2) + 2):
-        for j in range(2, len(str1) + 2):
-            matrix[1][i] = i - 1
-            matrix[j][1] = j - 1
+    for row in range(2, len(str1) + 2):
+        for column in range(2, len(str2) + 2):
+            pass
+            # matrix[row - 1][1] = row
+            # matrix[1][column] = row
 
 
-            print(str1[j-2], str2[i-2])
-            if str1[i - 2] == str2[j - 2]:
-                matrix[i][j] = matrix[i-1][j-1]
-            else:
-                pass
+            # print(str1[column-2], str2[column-2])
+            # if str1[column - 2] == str2[column - 2]:
+            #     matrix[row-1][column] = matrix[row-1][column-1]
+            # else:
+            #     pass
 
     return matrix
 
