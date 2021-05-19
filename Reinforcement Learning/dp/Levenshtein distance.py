388af83dca963c32f275a0d4dbe1fe9f5a4adff3
robb666 <56496611+robb666@users.noreply.github.com>
2021-05-20 00:00:13 +0200

    Update Levenshtein distance.py

--- Reinforcement Learning/dp/Levenshtein distance.py
+++ Reinforcement Learning/dp/Levenshtein distance.py
@@ -1,7 +1,38 @@
 import numpy as np
 
 
-def L_matrix(str1, str2):
+def L_distance(X, Y):
+    (m, n) = (len(X), len(Y))
+    T = [[0 for x in range(n + 1)] for y in range(m + 1)]
+
+    for i in range(1, m + 1):
+        T[i][0] = i
+
+    for j in range(1, n + 1):
+        T[0][j] = j
+
+    for row in range(1, m + 1):
+        for column in range(1, n + 1):
+
+            if X[row - 1] == Y[column - 1]:
+                cost = 0
+            else:
+                cost = 1
+            T[row][column] = min(int(T[row - 1][column] + 1),
+                                 int(T[row][column - 1] + 1),
+                                 int(T[row - 1][column - 1]) + cost)
+    return np.array(T)
+
+
+X = 'Robert'
+Y = 'Rafał'
+
+print(L_distance(X, Y))
+
+
+# Minimum Edit Distance
+#####################################################
+def minimum_edit_matrix(str1, str2):
     matrix = [[0 for _ in range(len(str1) + 2)] for _ in range(len(str1) + 2)]
     for row in range(len(str1)):
         for column in range(len(str2)):
@@ -13,12 +44,11 @@ def L_matrix(str1, str2):
     return np.array(matrix)
 
 
-def L_dist(str1, str2):
-    matrix = L_matrix(str1, str2)
+def minimum_edit_dist(str1, str2):
+    matrix = minimum_edit_matrix(str1, str2)
     for row in range(2, len(str1)+2):
         for column in range(2, len(str1)+2):
-            # print(matrix)
-            # print(row, column)
+
             if str1[row - 2] == str2[column - 2]:
                 matrix[row][column] = matrix[row - 1][column - 1]
                 print()
@@ -28,20 +58,18 @@ def L_dist(str1, str2):
                                           int(matrix[row - 1][column])) + 1
 
     for i in matrix:
-        for j in i:
-            print(j)
-            if j == ' ':
-                matrix = matrix[:-1]
+        if i[0] == ' ':
+            matrix = matrix[:np.where(matrix == ' ')[0][0]]
 
     return matrix
 
 
-str1 = 'abcdef'
-str2 = 'azced '
+str1 = 'Robert'
+str2 = 'Rafał '
 
-print(L_dist(str1, str2))
+print(minimum_edit_dist(str1, str2))
 # print(~1)
-# print(min(1, 0, 3, 5))
+
 
 
 
