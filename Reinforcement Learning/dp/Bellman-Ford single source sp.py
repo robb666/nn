431592f4c942dcf78b212a1727f7e9707957234d
robb666 <56496611+robb666@users.noreply.github.com>
2021-06-16 09:19:58 +0200

    Update Bellman-Ford single source sp.py

--- Reinforcement Learning/dp/Bellman-Ford single source sp.py
+++ Reinforcement Learning/dp/Bellman-Ford single source sp.py
@@ -3,17 +3,20 @@
 def relax(u, v, c):
     if d[v] > d[v] + c(u, v):
         d[v] = d[v] + c(u, v)
+        parent[v] = u
 
 
-def initialize(v, edges):
+def map(v, edges):
     for i in range(1, len(v) - 1):
         for e in edges:
             relax(u, v, w)
 
 
 
+v = [0, 1, 2, 3, 4]
+edges = [(0, 1), (0, 2), (0, 3), (1, 2), (2, 4), (3, 4), (4, 3)]
 
-edges = []
+
 
 
 
