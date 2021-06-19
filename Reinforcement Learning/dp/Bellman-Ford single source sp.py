acaad4a1b4557de2c04c97f03b2c2a1d184bd475
robb666 <56496611+robb666@users.noreply.github.com>
2021-06-19 09:48:31 +0200

    Update Bellman-Ford single source sp.py

--- Reinforcement Learning/dp/Bellman-Ford single source sp.py
+++ Reinforcement Learning/dp/Bellman-Ford single source sp.py
@@ -1,33 +1,55 @@
 
 
-def relax(dist, u, v, c):
+def relax(u, v, c):
+    # print(dist[u])
     if dist[u] + c < dist[v]:
         dist[v] = dist[u] + c
         # parent[v] = u
 
 
-def map(dist, vertex, edges):
+def map(vertex, edges):
     for _ in range(len(vertex) - 1):
         for u, v, c in edges:
-            relax(dist, u, v, c)
+            relax(u, v, c)
             
     return dist
 
 
-vertex = [0, 1, 2, 3, 4, 5]
-edges = [(0, 1, 4), (0, 2, 5), (0, 3, 6), (1, 2, -3),
-         (2, 5, 4), (3, 4, 2), (4, 5, 2), (5, 4, 1)]
+vertex = [1, 2, 3, 4, 5, 6]
+edges = [(1, 2, 6),
+         (1, 3, 5),
+         (1, 4, 5),
+         (2, 5, -1),
+         (3, 2, -2),
+         (3, 5, 1),
+         (4, 3, -2),
+         (4, 6, -1),
+         (5, 7, 3),
+         (6, 7, 3)]
 
-dist = [0, 1, 2, 3, 4, 5]
-dist[0] = 0
-dist[1] = float('inf')
+
+dist = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# dist[0] = 0
+dist[1] = 0
 dist[2] = float('inf')
 dist[3] = float('inf')
 dist[4] = float('inf')
 dist[5] = float('inf')
+dist[6] = float('inf')
+dist[7] = float('inf')
 
 
 print(dist)
 
-print(map(dist, vertex, edges))
+print(map(vertex, edges))
 
+
+
+
+# edges = [(0, 1, 4),
+#          (0, 2, 5),
+#          (0, 3, 8),
+#          (1, 2, -3),
+#          (2, 4, 4),
+#          (3, 4, 2),
+#          (4, 3, 1)]
