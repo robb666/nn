612d02daa8cbebf9cb901cf010df7aefe0280f04
Robb <56496611+robb666@users.noreply.github.com>
2022-04-05 21:01:30 +0200

    Update new_f.py

--- Machine Learning With PyTorch and Scikit-Learn/new_f.py
+++ Machine Learning With PyTorch and Scikit-Learn/new_f.py
@@ -17,7 +17,7 @@ print(df.tail())
 
 class Perceptron:
 
-    def init(self, eta=0.01, n_iter=50, random_state=1):
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
@@ -83,10 +83,10 @@ plt.show()
 
 
 
-model = Perceptron()
-
-arr = [1, 2, 3, 4, 5]
-X = np.dot(arr, 2) + 0.5
-
-print(X)
+# model = Perceptron()
+#
+# arr = [1, 2, 3, 4, 5]
+# X = np.dot(arr, 2) + 0.5
+#
+# print(X)
 
