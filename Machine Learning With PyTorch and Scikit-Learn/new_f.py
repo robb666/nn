440e2bd1e369592561580b68e34ff2a50e71c3d5
Robb <56496611+robb666@users.noreply.github.com>
2022-04-06 17:46:00 +0200

    Update new_f.py

--- Machine Learning With PyTorch and Scikit-Learn/new_f.py
+++ Machine Learning With PyTorch and Scikit-Learn/new_f.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 s = r'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -51,15 +52,15 @@ y = np.where(y == 'Iris-setosa', 0, 1)
 
 X = df.iloc[0:100, [0, 2]].values
 
-plt.scatter(X[:50, 0], X[:50, 1],
-            color='red', marker='o', label='setosa')
-
-plt.scatter(X[50:100, 0], X[50:100, 1],
-            color='blue', marker='s', label='versicolor')
-plt.xlabel('Sepal length [cm]')
-plt.ylabel('Petal length [cm]')
-plt.legend(loc='upper left')
-plt.show()
+# plt.scatter(X[:50, 0], X[:50, 1],
+#             color='red', marker='o', label='setosa')
+#
+# plt.scatter(X[50:100, 0], X[50:100, 1],
+#             color='blue', marker='s', label='versicolor')
+# plt.xlabel('Sepal length [cm]')
+# plt.ylabel('Petal length [cm]')
+# plt.legend(loc='upper left')
+# plt.show()
 
 
 
@@ -73,20 +74,37 @@ plt.ylabel('Number of updates')
 plt.show()
 
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.uniqe(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.uniqe(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=f'Class {cl}',
+                    edgecolor='black')
 
 
 
 
-
-
-
-
-
-
-# model = Perceptron()
-#
-# arr = [1, 2, 3, 4, 5]
-# X = np.dot(arr, 2) + 0.5
-#
-# print(X)
-
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('Sepal lenght [cm]')
+plt.ylabel('Petal lenght [cm]')
+plt.legend(loc='upper left')
+plt.show()
