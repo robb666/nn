4c623bd5831f564ade048afa20db4fb173bc10f0
Robb <56496611+robb666@users.noreply.github.com>
2022-05-05 14:38:26 +0200

    Update Introduction to PyTorch.py

--- The StatQuest/Introduction to PyTorch.py
+++ The StatQuest/Introduction to PyTorch.py
@@ -5,6 +5,7 @@ from torch.optim import SGD
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 
 class BasicNN(nn.Module):
@@ -33,65 +34,58 @@ class BasicNN(nn.Module):
         scaled_bottom_relu_output = top_relu_output * self.w11
 
         input_to_final_relu = scaled_top_relu_output + scaled_bottom_relu_output + self.final_bias
-        print('bias:', self.final_bias.data)
+        # print('bias:', self.final_bias.data)
         output = F.relu(input_to_final_relu)
 
         return output
 
 
+inputs = torch.tensor([0., 0.5, 1.])
+labels = torch.tensor([0., 1., 0.])
 
-
-# inputs = torch.tensor([0., 0.5, 1.])
-# labels = torch.tensor([0., 1., 0.])
-#
 model = BasicNN()
-#
-#
-# optimizer = SGD(model.parameters(), lr=0.1)
-#
-# print('Final bias, before optimization: ' + str(model.final_bias.data) + '\n')
-#
-# for epoch in range(100):
-#
-#     total_loss = 0
-#
-#     for iteration in range(len(inputs)):
-#
-#         input_i = inputs[iteration]
-#         label_i = labels[iteration]
-#
-#         output_i = model(input_i)
-#
-#         loss = (output_i - label_i) ** 2
-#
-#         loss.backward()
-#
-#         print(loss)
-#
-#         total_loss += float(loss)
-#
-#     if (total_loss < 0.0001):
-#         print('Num steps: ' + str(epoch))
-#         break
-#
-#     optimizer.step()
-#     optimizer.zero_grad()
-#
-#     print('Step: ' + str(epoch) + ' Final Bias: ' + str(model.final_bias.data) + '\n')
-#
-# print('Final bias, after optimization: ' + str(model.final_bias.data))
-#
-# PATH = r'C:\Users\Robert\Desktop\python\nn\The StatQuest\mod'
-# torch.save(model.state_dict(), PATH)
 
 
-# model = BasicNN()
-# model.load_state_dict(torch.load(PATH))
-# model.eval()
+optimizer = SGD(model.parameters(), lr=0.1)
+
+print('Final bias, before optimization: ' + str(model.final_bias.data) + '\n')
+
+for epoch in range(100):
+
+    total_loss = 0
+
+    for iteration in range(len(inputs)):
+
+        input_i = inputs[iteration]
+        label_i = labels[iteration]
+
+        output_i = model(input_i)
+
+        loss = (output_i - label_i) ** 2
+
+        loss.backward()
+
+        # print(loss)
+
+        total_loss += float(loss)
+
+    if total_loss < 0.0001:
+        print('Num steps: ' + str(epoch))
+        break
+
+    optimizer.step()
+    optimizer.zero_grad()
+
+    print('Step: ' + str(epoch) + ' Final Bias: ' + str(model.final_bias.data) + '\n')
+
+print('Final bias, after optimization: ' + str(model.final_bias.data))
+
 
 input_doses = torch.linspace(start=0, end=1, steps=11)
+print(input_doses)
 
 output_values = model(input_doses)
+print(output_values)
 
 sns.set(style='whitegrid')
 
