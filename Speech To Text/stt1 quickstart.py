ad491323c931185dfc114eea4b62289115294fd0
Robb <56496611+robb666@users.noreply.github.com>
2022-05-09 16:49:33 +0200

    Update stt1 quickstart.py

--- Speech To Text/stt1 quickstart.py
+++ Speech To Text/stt1 quickstart.py
@@ -1,45 +1,89 @@
 # Imports the Google Cloud client library
-from google.cloud import speech
-
-
-
+# from google.cloud import speech
 import os
-# from oauth2client.service_account import ServiceAccountCredentials
+from google.cloud import speech_v1 as speech
+
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = \
-    r'C:\Users\Robert\Desktop\python\nn\Speech to text API\text-to-speech-349604-369288fae48e.json'
+    r'C:\Users\PipBoy3000\Desktop\IT\projekty\nn\Speech To Text\text-to-speech-349604-9fb6a8e4da36.json'
 
-# scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-#          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-# creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
-# client = speech.SpeechClient.
-
-
+def speech_to_text(config, audio):
+    client = speech.SpeechClient()
+    response = client.recognize(config=config, audio=audio)
+    print_sentences(response)
 
 
+def print_sentences(response):
+    for result in response.results:
+        best_alternative = result.alternatives[0]
+        transcript = best_alternative.transcript
+        confidence = best_alternative.confidence
+        print("-" * 80)
+        print(f"Transcript: {transcript}")
+        print(f"Confidence: {confidence:.0%}")
 
 
-# Instantiates a client
-client = speech.SpeechClient()
-
-# The name of the audio file to transcribe
-gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
-
-audio = speech.RecognitionAudio(uri=gcs_uri)
-
+# config = dict(language_code="en-US")
 config = speech.RecognitionConfig(
     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code="en-US",  # 'pl-PL'
+    # sample_rate_hertz=16000,
+    language_code="pl-PL",  # 'pl-PL'
 )
+# audio = dict(uri="gs://cloud-samples-data/speech/brooklyn_bridge.flac")
+# audio = dict(uri="adres.wav")
+audio = dict(uri="gs://tts1_magro/nrrej.wav")
+speech_to_text(config, audio)
 
 
 
-# Detects speech in the audio file
-response = client.recognize(config=config, audio=audio)
-
-for result in response.results:
-    print("Transcript: {}".format(result.alternatives[0].transcript))
 
 
-# '111644870421302580539'
+
+
+
+
+
+
+
+
+
+# import os
+# # from oauth2client.service_account import ServiceAccountCredentials
+#
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = \
+#     r'C:\Users\PipBoy3000\Desktop\IT\projekty\nn\Speech To Text\
+#       client_secret_886959131153-vmbtcc6jrj5gg0vhijqpotroh7atr918.apps.googleusercontent.com.json'
+#
+# # scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+# #          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+#
+# "www.googleapis.com/speech.googleapis.com"
+# # creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+# # client = speech.SpeechClient.
+#
+#
+#
+#
+#
+#
+# # Instantiates a client
+# client = speech.SpeechClient()
+#
+# # The name of the audio file to transcribe
+# gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+#
+# audio = speech.RecognitionAudio(uri=gcs_uri)
+#
+# config = speech.RecognitionConfig(
+#     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+#     sample_rate_hertz=16000,
+#     language_code="en-US",  # 'pl-PL'
+# )
+#
+#
+#
+# # Detects speech in the audio file
+# response = client.recognize(config=config, audio=audio)
+#
+# for result in response.results:
+#     print("Transcript: {}".format(result.alternatives[0].transcript))
