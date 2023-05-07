8e29fce884ad06be62819c0b63439a31c9d96c25
robb666 <56496611+robb666@users.noreply.github.com>
2023-05-07 12:03:32 +0200

    Update car_ins_img.py

--- YOLOv5_cars/car_ins_img.py
+++ YOLOv5_cars/car_ins_img.py
@@ -15,6 +15,15 @@ TRAIN = True
 EPOCHS = 25
 
 
+def download_file(url, save_name):
+    url = url
+    if not os.path.exists(save_name):
+        file = requests.get(url)
+        open(save_name, 'wb').write(file.content)
+    else:
+        print('File already present, skipping download...')
+
+
 if not os.path.exists('roboflow_unzipped'):
     print('Downloading...')
     r = requests.get('https://public.roboflow.com/ds/xKLV14HbTF?key=aJzo7msVta')
@@ -35,17 +44,11 @@ if not os.path.exists('roboflow_unzipped'):
                 file_name = image_name.split('.jpg')[0]
                 os.remove(f'{dir_name}/images/{image_name}')
                 os.remove(f'{dir_name}/labels/{file_name}.txt')
-
 else:
     print('Skipping download. Folder exists.')
 
 class_names = ['Ambulance', 'Bus', 'Car', 'Motorcycle', 'Truck']
 colors = np.random.uniform(0, 255, size=(len(class_names), 3))
-
-
-
-
-
 
 
 def yolo2bbox(bboxes):
@@ -55,4 +58,117 @@ def yolo2bbox(bboxes):
 
 
 def plot_box(image, bboxes, labels):
-    pass
+    # Need the image height and width to denormalize the
+    # bounding box
+    h, w, _ = image.shape
+    for box_num, box in enumerate(bboxes):
+        x1, y1, x2, y2 = yolo2bbox(box)
+        # denormalize the coordinates
+        xmin = int(x1*w)
+        ymin = int(y1*h)
+        xmax = int(x2*w)
+        ymax = int(y2*h)
+        width = xmax - xmin
+        height = ymax - ymin
+
+        class_name = class_names[int(labels[box_num])]
+
+        cv2.rectangle(
+            image,
+            (xmin, ymin), (xmax, ymax),
+            color=colors[class_names.index(class_name)],
+            thickness=2
+        )
+
+        font_scale = min(1, max(3, int(w/500)))
+        font_thickness = min(2, max(10, int(w/50)))
+
+        p1, p2 = (int(xmin), int(ymin)), (int(xmax), int(ymax))
+        # Text width and height
+        tw, th = cv2.getTextSize(
+            class_name,
+            0, fontScale=font_scale, thickness=font_thickness
+        )[0]
+        p2 = p1[0] + tw, p1[1] + -th -10
+        cv2.rectangle(
+            image,
+            p1, p2,
+            color=colors[class_names.index(class_name)],
+            thickness=-1,
+        )
+        cv2.putText(
+            image,
+            class_name,
+            (xmin+1, ymin-10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            font_scale,
+            (225, 225, 225),
+            font_thickness
+        )
+    return image
+
+
+# Function to plot images with the bounding boxes
+def plot(image_paths, label_paths, num_samples):
+    all_training_images = glob.glob(image_paths)
+    all_training_labels = glob.glob(label_paths)
+    all_training_images.sort()
+    all_training_labels.sort()
+
+    num_images = len(all_training_images)
+
+    plt.figure(figsize=(15, 12))
+    for i in range(num_samples):
+        j = random.randint(0, num_images-1)
+        image = cv2.imread(all_training_images[j])
+        with open(all_training_labels[j], 'r') as f:
+            bboxes = []
+            labels = []
+            label_lines = f.readlines()
+            for label_line in label_lines:
+                label = label_line[0]
+                bbox_string = label_line[2:]
+
+                x_c, y_c, w, h = bbox_string.split(' ')
+                x_c = float(x_c)
+                y_c = float(y_c)
+                w = float(w)
+                h = float(h)
+                bboxes.append([x_c, y_c, w, h])
+                labels.append(label)
+        result_image = plot_box(image, bboxes, labels)
+        plt.subplot(2, 2, i+1)
+        plt.imshow(result_image[:, :, ::-1])
+        plt.axis('off')
+    plt.subplots_adjust(wspace=0)
+    plt.tight_layout()
+    plt.show()
+
+
+plot(image_paths='train/images/*',
+     label_paths='train/labels/*',
+     num_samples=4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
