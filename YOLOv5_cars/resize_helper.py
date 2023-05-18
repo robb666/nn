d52bd9045021e5aa3dde518e562962d567cda866
Robb <56496611+robb666@users.noreply.github.com>
2023-05-18 13:19:52 +0200

    Update resize_helper.py

---
+++ YOLOv5_cars/resize_helper.py
@@ -0,0 +1,26 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import os
+from pathlib import Path
+import glob
+
+
+
+# data_path = Path('~')
+data_path = Path('/home/robb/Desktop/labels yolo')
+
+
+print(data_path / 'zegary/Dash-Board-Lights-What-Do-They-Mean-1200x800-(5).webp')
+
+
+# # Load the image
+# image = Image.open('image.jpg')
+#
+# # Resize using PyTorch
+# resize_transform = transforms.Resize((224, 224))
+# resized_image = resize_transform(image)
+#
+# # Convert to tensor
+# tensor_image = transforms.ToTensor()(resized_image)
+
