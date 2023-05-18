a4e8fa3d0d8dd63ef191c9fd4b98ae499e111a99
Robb <56496611+robb666@users.noreply.github.com>
2023-05-18 14:01:00 +0200

    Update resize_helper.py

--- YOLOv5_cars/resize_helper.py
+++ YOLOv5_cars/resize_helper.py
@@ -4,23 +4,23 @@ from PIL import Image
 import os
 from pathlib import Path
 import glob
+import time
 
 
-
-# data_path = Path('~')
 data_path = Path('/home/robb/Desktop/labels yolo')
 
+image = data_path / 'zegary/3847fc_mv2.webp'
 
-print(data_path / 'zegary/Dash-Board-Lights-What-Do-They-Mean-1200x800-(5).webp')
 
+# Load the image
+image = Image.open(image)
 
-# # Load the image
-# image = Image.open('image.jpg')
-#
-# # Resize using PyTorch
-# resize_transform = transforms.Resize((224, 224))
-# resized_image = resize_transform(image)
-#
-# # Convert to tensor
-# tensor_image = transforms.ToTensor()(resized_image)
+# Resize using PyTorch
+resize_transform = transforms.Resize((224, 224))
+resized_image = resize_transform(image)
+
+# Convert to tensor
+tensor_image = transforms.ToTensor()(resized_image)
+
+print(tensor_image)
 
