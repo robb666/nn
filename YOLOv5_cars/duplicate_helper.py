209aecedfa6e2e13bf2b6f12f8b8114368dc0112
Robb <56496611+robb666@users.noreply.github.com>
2023-05-23 21:17:23 +0200

    Update duplicate_helper.py

--- YOLOv5_cars/duplicate_helper.py
+++ YOLOv5_cars/duplicate_helper.py
@@ -34,9 +34,12 @@ print()
 print()
 
 data_path = Path("/run/user/1000/gvfs/smb-share:server=192.168.1.12,share=e/zzzProjekty/labels yolo/dow rej")
+os.chdir(data_path)
+
 reg_certificates = os.listdir(data_path)
 
-os.chdir(data_path)
+print(reg_certificates)
+
 # subprocess.run(['python3', 'http.server', '8082'])
 
 # json_data = [
@@ -50,26 +53,26 @@ os.chdir(data_path)
 PORT = 8082
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Serving at port", PORT)
-
-    time.sleep(2)
-
-    # for img in reg_certificates:
-    if '20180607_065400.jpg' not in imgs_uploaded:
-        uu_id = str(uuid.uuid1()).split('-')[0]
-        json_data = [
-            {
-                'image': f'http://0.0.0.0:8082/{uu_id}-20180607_065400.jpg'
-            }
-        ]
-
-        print(json_data)
-        requests.post('http://localhost:8080/api/projects/1/import',
-                      headers={'Authorization': TOKEN},
-                      json=json_data).json()
-        
-        time.sleep(2)
+# with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#     print("Serving at port", PORT)
+#
+#     time.sleep(2)
+#
+#     # for img in reg_certificates:
+#     if '20180607_065400.jpg' not in imgs_uploaded:
+#         uu_id = str(uuid.uuid1()).split('-')[0]
+#         json_data = [
+#             {
+#                 'image': f'http://0.0.0.0:8082/{uu_id}-20180607_065400.jpg'
+#             }
+#         ]
+#
+#         print(json_data)
+#         requests.post('http://localhost:8080/api/projects/1/import',
+#                       headers={'Authorization': TOKEN},
+#                       json=json_data).json()
+#
+#         time.sleep(2)
 
 
 
