1ca1e44e3d6e63d6c592b043babda9a0cdc89bf0
Robb <56496611+robb666@users.noreply.github.com>
2023-05-31 08:41:13 +0200

    Update import_tasks.py

--- YOLOv5_cars/import_tasks.py
+++ YOLOv5_cars/import_tasks.py
@@ -8,32 +8,15 @@ import socketserver
 from creds import TOKEN
 
 
-class MultiDirHandler(http.server.SimpleHTTPRequestHandler):
-    data_path = Path("/run/user/1000/gvfs/smb-share:server=192.168.1.12,share=e/zzzProjekty/labels yolo")
-    directories = [
-        data_path / 'dow rej',
-        data_path / 'kluczyki',
-    ]
-
-    def translate_path(self, path):
-        # Map path to the corresponding file in multiple directories
-        for directory in self.directories:
-            full_path = os.path.join(directory, path[1:])
-            if os.path.exists(full_path):
-                return full_path
-        return super().translate_path(path)
-
-
 def merge(all_images, uploaded_images):
     PORT = 8082
-    # Handler = http.server.SimpleHTTPRequestHandler
-    Handler = MultiDirHandler
+    Handler = http.server.SimpleHTTPRequestHandler
+
     i = 0
     print('top', os.getcwd())
     with socketserver.TCPServer(('', PORT), Handler) as httpd:
         print('\nServing at port:', PORT)
         for img in all_images:
-            print(os.getcwd())
             if img not in uploaded_images and img not in ('Thumbs.db', 'desktop.ini'):
                 json_data = {'image': f'http://0.0.0.0:{PORT}/{img}'}
 
