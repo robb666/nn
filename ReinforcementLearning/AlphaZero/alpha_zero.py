4f543ae031bb87e801e49acf684eca15952380bb
robb666 <56496611+robb666@users.noreply.github.com>
2023-07-23 18:33:00 +0200

    Update alpha_zero.py

--- ReinforcementLearning/AlphaZero/alpha_zero.py
+++ ReinforcementLearning/AlphaZero/alpha_zero.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 print(np.__version__)
 
@@ -21,6 +22,9 @@ class TicTacToe:
         return (state.reshape(-1) == 0).astype(np.uint8)
 
     def check_win(self, state, action):
+        if action is None:
+            return False
+
         row = action // self.column_count
         column = action % self.column_count
         player = state[row, column]
@@ -37,9 +41,14 @@ class TicTacToe:
             return 1, True
         if np.sum(self.get_valid_moves(state)) == 0:
             return 0, True
+        return 0, False
 
     def get_opponent(self, player):
         return -player
+
+    def get_opponent_value(self):
+        return -value
+
 
 
 tictactoe = TicTacToe()
@@ -51,16 +60,97 @@ state = tictactoe.get_initial_state()
 while True:
     print(state)
     valid_moves = tictactoe.get_valid_moves(state)
-    print("valid_moves", [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
-    action = int(input())
+    print("Valid_moves: ", [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
+    action = int(input(str(player) + ': '))
+
+    if valid_moves[action] == 0:
+        print('Action not valid')
+        continue
+
+    state = tictactoe.get_next_state(state, action, player)
+
+    value, is_terminal = tictactoe.get_value_and_terminated(state, action)
+
+    if is_terminal:
+        print(state)
+        if value == 1:
+            print(player, 'Won!')
+        else:
+            print('Draw')
+        break
+
+    player = tictactoe.get_opponent(player)
+
+
+class Node:
+    def __init__(self, game, args, state, parent=None, action_taken=None):
+        self.game = game
+        self.args = args
+        self.state = state
+        self.parent = parent
+        self.action = action_taken
+
+        self.children = []
+        self.expandable_moves = game.get_valid_moves(state)
+
+        self.visit_count = 0
+        self.value_sum = 0
+
+    def is_fully_expanded(self):
+        return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
+
+    def select(self):
+        best_child = None
+        best_ucb = -np.inf
+
+        for child in self.children:
+            ucb = self.get_ucb(child)
+            if ucb > best_ucb:
+                best_child = child
+                best_ucb = ucb
+
+        return best_child
+
+    def get_ucb(self, child):
+        q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
+        return q_value + self.args['C'] * math.sqrt(math.log(self.visit_count) / child.visit_count)
+
+    def expand(self):
+        action = np.random.choice(np.where(self.expandable_moves == 1)[0])
+        self.expandable_moves[action] = 0
+
+        child_state = self.state.copy()
+        child_state = self.game.get_next_state(child_state, action, 1)
 
 
 
+class MCTS:
+    def __init__(self, game, args):
+        self.game = game
+        self.args = args
 
+    def search(self, state):
+        # define root
+        root = Node(self.game, self.args, state)
 
+        for search in range(self.args['num_searches']):
+            node = root
 
+            while node.is_fully_expanded():
+                # selection
+                node = node.select()
 
+            value, is_terminal = self.game.get_value_and_terminated(node.state, node.action_taken)
+            value = self.game.get_opponent_value(value)
 
+            if not is_terminal:
+                node = node.expand()
+                # expansion
+
+            # simulation
+            # backpropagation
+
+        # return visit_count
 
 
 
