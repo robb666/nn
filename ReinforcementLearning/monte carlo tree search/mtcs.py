4c59a4eb9450b27fefedc9aaaec41b79ffa9e67f
Robb <56496611+robb666@users.noreply.github.com>
2023-07-31 05:15:19 +0200

    1

---
+++ ReinforcementLearning/monte carlo tree search/mtcs.py
@@ -0,0 +1,53 @@
+"""The monte carlo tree search"""
+
+'1. - it takes the current game state'
+
+import random
+import ast
+
+userPlayer = '0'
+boardSize = 3
+numberOfSimulations = 200
+
+board = [
+    list('...'),
+    list('...'),
+    list('...')
+]
+
+startingPlayer = 'x'
+currentPlayer = startingPlayer
+
+def getBoardCopy(board):
+    boardCopy = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
