a871faa4b199e626da83fa5eec8c02d43c98902b
robb666 <56496611+robb666@users.noreply.github.com>
2023-11-20 20:36:37 +0100

    Update alpha_zero.py

--- ReinforcementLearning/AlphaZero/alpha_zero.py
+++ ReinforcementLearning/AlphaZero/alpha_zero.py
@@ -295,10 +295,51 @@ class AlphaZero:
         self.mcts = MCTS(game, args, model)
 
     def selfPlay(self):
+        memory = []
+        player = 1
+        state = self.game.get_initial_state()
+
+        while True:
+            neutral_state = self.game.change_perspective(state, player)
+            action_probs = self.mcts.search(neutral_state)
+
+            memory.append((neutral_state, action_probs, player))
+
+            action = np.random.choice(self.game.action_size, p=action_probs)
+
+            state = self.game.get_next_state(state, action, player)
+
+            value, is_terminal = self.game.get_value_and_terminated(state, action)
+
+            if is_terminal:
+                returnMemory = []
+                for hist_neutral_state, hist_action_probs, hist_player in memory:
+                    hist_outcome = value if hist_player == player else -value
+                    returnMemory.append((
+                        self.game.get_encoded_state(hist_neutral_state),
+                        hist_action_probs,
+                        hist_outcome
+                    ))
+                return returnMemory
+
+
+    def train(self, memory):
         pass
 
-    def train(self):
-        pass
+    def learn(self):
+        for iteration in self.args['num_iterations']:
+            memory = []
+
+            self.model.eval()
+            for selfPlay_iteration in range(self.args['num_selfPlay_iterations']):
+                memory += self.selfPlay()
+
+            self.model.train()
+            for epoch in range(self.args['num_epochs']):
+                self.train(memory)
+
+            torch.save(self.model.state_dict(), f'model_{iteration}.pt')
+            torch.save(self.optimizer.state_dict(), f'optimizer_{iteration}.pt')
 
 
 
