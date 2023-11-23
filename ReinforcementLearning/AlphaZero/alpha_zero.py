f7061f7fad38b6bcc76e13f3dfe26aa164bcf6cb
robb666 <56496611+robb666@users.noreply.github.com>
2023-11-23 11:28:58 +0100

    Update alpha_zero.py

--- ReinforcementLearning/AlphaZero/alpha_zero.py
+++ ReinforcementLearning/AlphaZero/alpha_zero.py
@@ -20,6 +20,9 @@ class TicTacToe:
         self.column_count = 3
         self.action_size = self.row_count * self.column_count
 
+    def __repr__(self):
+        return 'TicTacToe'
+
     def get_initial_state(self):
         return np.zeros((self.row_count, self.column_count))
 
@@ -45,6 +48,80 @@ class TicTacToe:
             or np.sum(state[:, column]) == player * self.row_count
             or np.sum(np.diag(state)) == player * self.row_count
             or np.sum(np.diag(np.flip(state, axis=0))) == player * self.row_count
+        )
+
+    def get_value_and_terminated(self, state, action):
+        if self.check_win(state, action):
+            return 1, True
+        if np.sum(self.get_valid_moves(state)) == 0:
+            return 0, True
+        return 0, False
+
+    def get_opponent(self, player):
+        return -player
+
+    def get_opponent_value(self, value):
+        return -value
+
+    def change_perspective(self, state, player):
+        return state * player
+
+    def get_encoded_state(self, state):
+        encoded_state = np.stack(
+            (state == -1, state == 0, state == 1)
+        ).astype(np.float32)
+        return encoded_state
+
+
+class ConnectFour:
+    def __init__(self):
+        self.row_count = 6
+        self.column_count = 7
+        self.action_size = self.column_count
+        self.in_a_row = 4
+
+    def __repr__(self):
+        return 'ConnectFour'
+
+    def get_initial_state(self):
+        return np.zeros((self.row_count, self.column_count))
+
+    def get_next_state(self, state, action, player):
+        row = np.max(np.where(state[:, action] == 0))
+        state[row, action] = player
+        return state
+
+    def get_valid_moves(self, state):
+        return (state[0] == 0).astype(np.uint8)
+
+    def check_win(self, state, action):
+        if action is None:
+            return False
+
+        row = np.min(np.where(state[:, action] != 0))
+        column = action
+        player = state[row][column]
+
+        def count(offset_row, offset_column):
+            for i in range(1, self.in_a_row):
+                r = row + offset_row * i
+                c = action + offset_column * i
+                if (
+                    r < 0
+                    or r >= self.row_count
+                    or c < 0
+                    or c >= self.column_count
+                    or state[r][c] != player
+                ):
+
+                    return i - 1
+            return self.in_a_row - 1
+
+        return (
+            count(1, 0) >= self.in_a_row - 1  # vertical
+            or (count(0, 1) + count(0, -1)) >= self.in_a_row - 1  # horizontal
+            or (count(1, 1) + count(-1, -1)) >= self.in_a_row - 1  # top left diagonal
+            or (count(1, -1) + count(-1, 1)) >= self.in_a_row - 1  # top right diagonal
         )
 
     def get_value_and_terminated(self, state, action):
@@ -129,37 +206,37 @@ class ResBlock(nn.Module):
         return x
 
 
-import matplotlib.pyplot as plt
-
-tictactoe = TicTacToe()
-
-state = tictactoe.get_initial_state()
-state = tictactoe.get_next_state(state, 2, 1)
-state = tictactoe.get_next_state(state, 7, -1)
-
-print(state)
-
-encoded_state = tictactoe.get_encoded_state(state)
-print(encoded_state)
-
-tensor_state = torch.tensor(encoded_state).unsqueeze(0)
-
-model = ResNet(tictactoe, 4, 64, device=torch.device('cpu'))
+# import matplotlib.pyplot as plt
+#
+# tictactoe = TicTacToe()
+#
+# state = tictactoe.get_initial_state()
+# state = tictactoe.get_next_state(state, 2, 1)
+# state = tictactoe.get_next_state(state, 7, -1)
+#
+# print(state)
+#
+# encoded_state = tictactoe.get_encoded_state(state)
+# print(encoded_state)
+#
+# tensor_state = torch.tensor(encoded_state).unsqueeze(0)
+#
+# model = ResNet(tictactoe, 4, 64, device=torch.device('cpu'))
+# # policy, value = model(tensor_state)
+# model.load_state_dict(torch.load('model_2.pt'))
+# model.eval()
+#
+# # value = value.item()
+# # policy = torch.softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
 # policy, value = model(tensor_state)
-model.load_state_dict(torch.load('model_2.pt'))
-model.eval()
-
 # value = value.item()
 # policy = torch.softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
-policy, value = model(tensor_state)
-value = value.item()
-policy = torch.softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
-
-
-print(value, policy, state, tensor_state)
-
-plt.bar(range(tictactoe.action_size), policy)
-plt.show()
+#
+#
+# print(value, policy, state, tensor_state)
+#
+# plt.bar(range(tictactoe.action_size), policy)
+# plt.show()
 
 
 
@@ -385,31 +462,180 @@ class AlphaZero:
             for epoch in trange(self.args['num_epochs']):
                 self.train(memory)
 
-            torch.save(self.model.state_dict(), f'model_{iteration}.pt')
-            torch.save(self.optimizer.state_dict(), f'optimizer_{iteration}.pt')
+            torch.save(self.model.state_dict(), f'model_{iteration}_{self.game}.pt')
+            torch.save(self.optimizer.state_dict(), f'optimizer_{iteration}_{self.game}.pt')
 
 
-tictactoe = TicTacToe()
+class MCTSParallel:
+    def __init__(self, game, args, model):
+        self.game = game
+        self.args = args
+        self.model = model
+
+    @torch.no_grad()
+    def search(self, state):
+        # define root
+        root = Node(self.game, self.args, state, visit_count=1)
+
+        policy, _ = self.model(
+            torch.tensor(self.game.get_encoded_state(state), device=self.model.device).unsqueeze(0)
+        )
+        policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+
+        policy = (1 - self.args['dirichlet_epsilon']) * policy + self.args['dirichlet_epsilon'] \
+                 * np.random.dirichlet([self.args['dirichlet_alpha']] * self.game.action_size)
+
+        valid_moves = self.game.get_valid_moves(state)
+        policy *= valid_moves
+        policy /= np.sum(policy)
+
+        for search in range(self.args['num_searches']):
+            node = root
+
+            while node.is_fully_expanded():
+                # selection
+                node = node.select()
+
+            value, is_terminal = self.game.get_value_and_terminated(node.state, node.action_taken)
+            value = self.game.get_opponent_value(value)
+
+            if not is_terminal:
+
+                policy, value = self.model(
+                    torch.tensor(self.game.get_encoded_state(node.state), device=self.model.device).unsqueeze(0)
+                )
+                # policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+                policy = torch.softmax(policy, axis=1).squeeze(0).detach().numpy()
+                valid_moves = self.game.get_valid_moves(node.state)
+                policy *= valid_moves
+                policy /= np.sum(policy)
+                # # expansion
+                # node = node.expand()
+
+
+                value = value.item()
+                node.expand(policy)
+                # # simulation
+                # value = node.simulate()
+
+
+            # backpropagation
+            node.backpropagate(value)
+
+        action_probs = np.zeros(self.game.action_size)
+        for child in root.children:
+            action_probs[child.action_taken] = child.visit_count
+        action_probs /= np.sum(action_probs)
+        return action_probs
+
+
+class AlphaZeroParallel:
+    def __init__(self, model, optimizer, game, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.game = game
+        self.args = args
+        self.mcts = MCTS(game, args, model)
+
+    def selfPlay(self):
+        return_memory = []
+        player = 1
+        spGames = [SPG(self.game) for spg in range(self.args['num_parallel_games'])]  ## koniec
+
+        while True:
+            neutral_state = self.game.change_perspective(state, player)
+            action_probs = self.mcts.search(neutral_state)
+
+            memory.append((neutral_state, action_probs, player))
+
+            temperature_action_probs = action_probs ** (1 / self.args['temperature'])
+            action = np.random.choice(self.game.action_size, p=temperature_action_probs)
+
+            state = self.game.get_next_state(state, action, player)
+
+            value, is_terminal = self.game.get_value_and_terminated(state, action)
+
+            if is_terminal:
+                returnMemory = []
+                for hist_neutral_state, hist_action_probs, hist_player in memory:
+                    hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
+                    returnMemory.append((
+                        self.game.get_encoded_state(hist_neutral_state),
+                        hist_action_probs,
+                        hist_outcome
+                    ))
+                return returnMemory
+
+            player = self.game.get_opponent(player)
+
+    def train(self, memory):
+        random.shuffle(memory)
+        for batchIdx in range(0, len(memory), self.args['batch_size']):
+            sample = memory[batchIdx:min(len(memory) - 1, batchIdx + self.args['batch_size'])]
+            state, policy_targets, value_targets = zip(*sample)
+
+            state, policy_targets, value_targets = np.array(state), np.array(policy_targets), np.array(value_targets).reshape(-1, 1)
+
+            state = torch.tensor(state, dtype=torch.float32, device=self.model.device)
+            policy_targets = torch.tensor(policy_targets, dtype=torch.float32, device=self.model.device)
+            value_targets = torch.tensor(value_targets.float32, device=self.model.device)
+
+            out_policy, out_value = self.model(state)
+
+            policy_loss = F.cross_entropy(out_policy, policy_targets)
+            value_loss = F.mse_loss(out_value, value_targets)
+            loss = policy_loss + value_loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    def learn(self):
+        for iteration in self.args['num_iterations']:
+            memory = []
+
+            self.model.eval()
+            for selfPlay_iteration in trange(self.args['num_selfPlay_iterations'] // self.args['num_parallel_games']):
+                memory += self.selfPlay()
+
+            self.model.train()
+            for epoch in trange(self.args['num_epochs']):
+                self.train(memory)
+
+            torch.save(self.model.state_dict(), f'model_{iteration}_{self.game}.pt')
+            torch.save(self.optimizer.state_dict(), f'optimizer_{iteration}_{self.game}.pt')
+
+
+class SPG:
+    def __init__(self, game):
+        self.state = game.get_initial_state()
+        self.memory = []
+        self.root = None
+        self.node = Node
+
+
+game = ConnectFour()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = ResNet(tictactoe, 4, 64, device)
+model = ResNet(game, 9, 128, device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001 )
 
 args = {
     'C': 2,
-    'num_searches': 60,
-    'num_iterations': 3,
+    'num_searches': 600,
+    'num_iterations': 8,
     'num_selfPlay_iterations': 500,
+    'num_parallel_games': 100,
     'num_epochs': 4,
-    'batch_size': 64,
+    'batch_size': 128,
     'temperature': 1.25,
     'dirichlet_epsilon': 0.25,
     'dirichlet_alpha': 0.3,
 }
 
-alphaZero = AlphaZero(model, optimizer, tictactoe, args)
+alphaZero = AlphaZero(model, optimizer, game, args)
 alphaZero.learn()
 
 
@@ -420,12 +646,12 @@ alphaZero.learn()
 
 # tictactoe = TicTacToe()
 # player = 1
-#
+
 # args = {
 #     'C': 1.41,
-#     'num_searches': 1000
+#     'num_searches': 1000,
 # }
-#
+
 # model = ResNet(tictactoe, 4, 64)
 # mcts = MCTS(tictactoe, args, model)
 # state = tictactoe.get_initial_state()
@@ -463,3 +689,61 @@ alphaZero.learn()
 #         break
 #
 #     player = tictactoe.get_opponent(player)
+
+
+
+
+# game = TicTacToe()
+game = ConnectFour()
+player = 1
+
+args = {
+    'C': 2,
+    'num_searches': 100,
+    'dirichlet_epsilon': 0.0,
+    'dirichlet_alpha': 0.3,
+}
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = ResNet(game, 9, 128, device)
+model.load_state_dict(torch.load('model_0_ConnectFour.pt', map_location=device))
+model.eval()
+
+mcts = MCTS(game, args, model)
+
+state = game.get_initial_state()
+
+
+while True:
+    print(state)
+
+    if player == 1:
+
+        valid_moves = game.get_valid_moves(state)
+        print("Valid_moves: ", [i for i in range(game.action_size) if valid_moves[i] == 1])
+        action = int(input(str(player) + ': '))
+
+        if valid_moves[action] == 0:
+            print('Action not valid')
+            continue
+
+    else:
+        neutral_state = game.change_perspective(state, player)
+        mcts_probs = mcts.search(state)
+        action = np.argmax(mcts_probs)
+
+    state = game.get_next_state(state, action, player)
+
+    value, is_terminal = game.get_value_and_terminated(state,
+                                                       action)
+
+    if is_terminal:
+        print(state)
+        if value == 1:
+            print(player, 'Won!')
+        else:
+            print('Draw')
+        break
+
+    player = game.get_opponent(player)
