7489a5cd2165695731a0d327e32047ed210a5ad9
robb666 <56496611+robb666@users.noreply.github.com>
2023-11-23 18:48:28 +0100

    Update alpha_zero.py

--- ReinforcementLearning/AlphaZero/alpha_zero.py
+++ ReinforcementLearning/AlphaZero/alpha_zero.py
@@ -144,6 +144,10 @@ class ConnectFour:
         encoded_state = np.stack(
             (state == -1, state == 0, state == 1)
         ).astype(np.float32)
+
+        if len(state.shape) == 3:
+            encoded_state = np.swapaxes(encoded_state, 0, 1)
+
         return encoded_state
 
 
@@ -473,60 +477,64 @@ class MCTSParallel:
         self.model = model
 
     @torch.no_grad()
-    def search(self, state):
-        # define root
-        root = Node(self.game, self.args, state, visit_count=1)
-
+    def search(self, states, spGames):
         policy, _ = self.model(
-            torch.tensor(self.game.get_encoded_state(state), device=self.model.device).unsqueeze(0)
+            torch.tensor(self.game.get_encoded_state(states), device=self.model.device).unsqueeze(0)
         )
-        policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
+        policy = torch.softmax(policy, axis=1).cpu().numpy()
 
         policy = (1 - self.args['dirichlet_epsilon']) * policy + self.args['dirichlet_epsilon'] \
-                 * np.random.dirichlet([self.args['dirichlet_alpha']] * self.game.action_size)
+                 * np.random.dirichlet([self.args['dirichlet_alpha']] * self.game.action_size, size=policy.shape[0])
 
-        valid_moves = self.game.get_valid_moves(state)
-        policy *= valid_moves
-        policy /= np.sum(policy)
+        for i, spg in enumerate(spGames):
+            spg_policy = policy[i]
+            valid_moves = self.game.get_valid_moves(states[i])
+            spg_policy *= valid_moves
+            spg_policy /= np.sum(spg_policy)
+
+            spg.root = Node(self.game, self.args, states[i], visit_count=1)
+            spg.root.expand(spg_policy)
 
         for search in range(self.args['num_searches']):
-            node = root
+            for spg in spGames:
+                spg.node = None
+                node = spg.root
 
-            while node.is_fully_expanded():
-                # selection
-                node = node.select()
+                while node.is_fully_expanded():
+                    # selection
+                    node = node.select()
 
-            value, is_terminal = self.game.get_value_and_terminated(node.state, node.action_taken)
-            value = self.game.get_opponent_value(value)
+                value, is_terminal = self.game.get_value_and_terminated(node.state, node.action_taken)
+                value = self.game.get_opponent_value(value)
 
-            if not is_terminal:
+                if is_terminal:
+                    # backpropagation
+                    node.backpropagate(value)
+                else:
+                    spg.node = node
+
+            expandable_spGames = [mappingIdx for mappingIdx in range(len(spGames)) is spGames[mappingIdx].node is not None]
+
+            if len(expandable_spGames) > 0:
+                states = np.stack([spGames[mappingIdx].node.state for mappingIdx in expandable_spGames])
 
                 policy, value = self.model(
-                    torch.tensor(self.game.get_encoded_state(node.state), device=self.model.device).unsqueeze(0)
+                    torch.tensor(self.game.get_encoded_state(states), device=self.model.device)
                 )
                 # policy = torch.softmax(policy, axis=1).squeeze(0).cpu().numpy()
-                policy = torch.softmax(policy, axis=1).squeeze(0).detach().numpy()
+                # policy = torch.softmax(policy, axis=1).squeeze(0).detach().numpy()
+                value = value.cpu().numpy()
+
+            for i, mappingIdx in enumerate(expandable_spGames):
+                node = spGames[mappingIdx].node
+                spg_policy, spg_value = policy[i], value[i]
+
                 valid_moves = self.game.get_valid_moves(node.state)
-                policy *= valid_moves
-                policy /= np.sum(policy)
-                # # expansion
-                # node = node.expand()
+                spg_policy *= valid_moves
+                spg_value /= np.sum(spg_policy)
 
-
-                value = value.item()
-                node.expand(policy)
-                # # simulation
-                # value = node.simulate()
-
-
-            # backpropagation
-            node.backpropagate(value)
-
-        action_probs = np.zeros(self.game.action_size)
-        for child in root.children:
-            action_probs[child.action_taken] = child.visit_count
-        action_probs /= np.sum(action_probs)
-        return action_probs
+                node.expand(spg_policy)
+                node.backpropagate(spg_value)
 
 
 class AlphaZeroParallel:
@@ -535,38 +543,50 @@ class AlphaZeroParallel:
         self.optimizer = optimizer
         self.game = game
         self.args = args
-        self.mcts = MCTS(game, args, model)
+        self.mcts = MCTSParallel(game, args, model)
 
     def selfPlay(self):
         return_memory = []
         player = 1
-        spGames = [SPG(self.game) for spg in range(self.args['num_parallel_games'])]  ## koniec
+        spGames = [SPG(self.game) for spg in range(self.args['num_parallel_games'])]
 
-        while True:
-            neutral_state = self.game.change_perspective(state, player)
-            action_probs = self.mcts.search(neutral_state)
+        while len(spGames) > 0:
+            states = np.stack([spg.state for spg in spGames])
+            neutral_states = self.game.change_perspective(states, player)
 
-            memory.append((neutral_state, action_probs, player))
+            self.mcts.search(neutral_states, spGames)
 
-            temperature_action_probs = action_probs ** (1 / self.args['temperature'])
-            action = np.random.choice(self.game.action_size, p=temperature_action_probs)
+            for i in range(len(spGames)):
+                spg = spGames[i]
 
-            state = self.game.get_next_state(state, action, player)
+                action_probs = np.zeros(self.game.action_size)
+                for child in spg.root.children:
+                    action_probs[child.action_taken] = child.visit_count
+                action_probs /= np.sum(action_probs)
 
-            value, is_terminal = self.game.get_value_and_terminated(state, action)
+                spg.memory.append((spg.root.state, action_probs, player)) #neutral_states?
 
-            if is_terminal:
-                returnMemory = []
-                for hist_neutral_state, hist_action_probs, hist_player in memory:
-                    hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
-                    returnMemory.append((
-                        self.game.get_encoded_state(hist_neutral_state),
-                        hist_action_probs,
-                        hist_outcome
-                    ))
-                return returnMemory
+                temperature_action_probs = action_probs ** (1 / self.args['temperature'])
+                action = np.random.choice(self.game.action_size, p=temperature_action_probs)
+
+                spg.state = self.game.get_next_state(spg.state, action, player)
+
+                value, is_terminal = self.game.get_value_and_terminated(spg.state, action)
+
+                if is_terminal:
+                    for hist_neutral_state, hist_action_probs, hist_player in spg.memory:
+                        hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
+                        return_memory.append((
+                            self.game.get_encoded_state(hist_neutral_state),
+                            hist_action_probs,
+                            hist_outcome
+                        ))
+
+                    del spGames[i]
 
             player = self.game.get_opponent(player)
+
+        return return_memory
 
     def train(self, memory):
         random.shuffle(memory)
@@ -611,7 +631,7 @@ class SPG:
         self.state = game.get_initial_state()
         self.memory = []
         self.root = None
-        self.node = Node
+        self.node = None
 
 
 game = ConnectFour()
@@ -635,7 +655,7 @@ args = {
     'dirichlet_alpha': 0.3,
 }
 
-alphaZero = AlphaZero(model, optimizer, game, args)
+alphaZero = AlphaZeroParallel(model, optimizer, game, args)
 alphaZero.learn()
 
 
