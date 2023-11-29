8227f9b90db5ea599bd7ca4d6636061e997d0dff
Robb <56496611+robb666@users.noreply.github.com>
2023-11-29 18:27:05 +0100

    up

--- ReinforcementLearning/lunar lander/LunarLander2020.py
+++ ReinforcementLearning/lunar lander/LunarLander2020.py
@@ -8,13 +8,12 @@ from icecream import ic
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2', render_mode="human")
-    print(env.)
+    print(env.reset())
     agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4,
-                  eps_end=0.01, input_dims=[8, 8], lr=0.003)
+                  eps_end=0.01, input_dims=[8], lr=0.003)
     scores, eps_history = [], []
     n_games = 100
     for i in range(n_games):
-
         score = 0
         done = False
         observation = env.reset()
