2e37fdae4da8ec5444adfb552ab20f690aa72be1
oobx <robert.patryk.grzelak@gmail.com>
2024-01-21 22:12:43 +0100

    up

--- ReinforcementLearning/self/pynput_tests.py
+++ ReinforcementLearning/self/pynput_tests.py
@@ -11,27 +11,40 @@ import random
 
 class Agent:
 
-    def __init__(self):
+    def __init__(self, details):
         self.kb = Controller()
         self.mouse = MouseController()
         self.pol_chars = {'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż', 'Ą', 'Ć', 'Ę', 'Ł', 'Ń', 'Ó', 'Ś', 'Ź', 'Ż'}
+
+        self.actions = ['enter', 'tab', 'tab_up', 'up', 'down', 'back', 'forth', 'esc',
+                        # 'close_tab',
+                        'copy_paste', 'type_randomly', 'click_kb', 'mouse_right', 'mouse_left', 'mouse_down',
+                        'mouse_up', 'click', 'delete', 'wait_0', 'wait_1']
+
+    def choose_and_execute_action(self):
+        action_name = random.choice(self.actions)
+        print(action_name)
+        action = getattr(self, action_name)
+        if action_name in ['type_randomly', 'copy_paste']:
+            action("Example text")
+        else:
+            action()
 
     def enter(self):
         self.kb.tap(Key.enter)
 
     def tab(self):
         self.kb.tap(Key.tab)
-        # self.kb.Key.tab
 
     def tab_up(self):
         with self.kb.pressed(Key.shift):
             self.kb.tap(Key.tab)
 
     def up(self):
-        self.kb(Key.up)
+        self.kb.tap(Key.up)
 
     def down(self):
-        self.kb(Key.down)
+        self.kb.tap(Key.down)
 
     def back(self):
         with self.kb.pressed(Key.alt_l):
@@ -44,9 +57,9 @@ class Agent:
     def esc(self):
         self.kb.tap(Key.esc)
 
-    def close_tab(self):
-        with self.kb.pressed(Key.ctrl):
-            self.kb.tap('w')
+    # def close_tab(self):
+    #     with self.kb.pressed(Key.ctrl):
+    #         self.kb.tap('w')
 
     def copy_paste(self, var):
         pyperclip.copy(var)
@@ -63,10 +76,28 @@ class Agent:
                 self.kb.tap(letter)
             sleep(random.uniform(.06, .29))
 
-    def click(self):  # TODO how to click in tab
-        self.mouse.click(Button.left)
+    def click_kb(self):
+        self.kb.tap(Key.space)
 
-    # TODO del method
+    def mouse_right(self, x=20, y=0):
+        self.mouse.move(x, y)
+
+    def mouse_left(self, x=-20, y=0):
+        self.mouse.move(x, y)
+
+    def mouse_down(self, x=0, y=20):
+        self.mouse.move(x, y)
+
+    def mouse_up(self, x=0, y=-20):
+        self.mouse.move(x, y)
+
+    def click(self):
+        self.mouse.click(Button.left, 1)
+
+    def delete(self):
+        with self.kb.pressed(Key.ctrl):
+            self.kb.tap('a')
+        self.kb.tap(Key.delete)
 
     def wait_0(self):
         sleep(random.uniform(.05, .12))
@@ -80,35 +111,15 @@ class Agent:
     #             self.kb.tap(key)
 
 
-data = ['6062815954', 'robert.try12@gmail.com']
+# data = ['6062815954', 'robert.try12@gmail.com']
 
-# sleep(4)
-A = Agent()
-
-
-A.copy_paste(data[0])
-
-A.tab()
-A.tab()
-
-A.type_randomly(data[1])
-
-A.tab()
-
-A.type_randomly(data[1])
-
-A.tab()
-A.tab()
-
-A.enter()
-
-A.back()
-
-sleep(3)
-
-A.forth()
+sleep(4)
 
 
+A = Agent(details=None)
+
+for _ in range(10):
+    A.choose_and_execute_action()
 
 
 
@@ -116,137 +127,5 @@ A.forth()
 #     pyautogui.hotkey("ctrl", "shift", "j")
 #     kb.press(Key.end)
 #     sleep(1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
