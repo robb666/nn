a67688263eafb116e8823b7411d3f5f934235976
oobx <robert.patryk.grzelak@gmail.com>
2024-02-09 11:40:37 +0100

    up

--- ReinforcementLearning/by book/policy_iteration.py
+++ ReinforcementLearning/by book/policy_iteration.py
@@ -1,13 +1,22 @@
 import numpy as np
+import random
 
-S = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+S =   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+S_prim = [1, 2, 3, 4, 5, 6, 7, 9, 10]
 
 
 def V(s):
 	if s == 9:
-		return 2, 2
+		return 2
 	else:
-		return 1, 1
+		return 1
+
+
+def step(s):
+	if s == 8:
+		return 9
+	else:
+		return random.choice([i for i in S_prim if i not in [9]])
 
 
 theta = .01
@@ -15,11 +24,10 @@ gamma = .9
 
 
 while True:
-	delta = -np.inf
+	delta = 0
 	for s in S:
 		v, r = V(s)
-		V = V + [r + gamma * V(s_prim)]
+		V += [r + gamma * V(step(s))]
 		delta = max(delta, abs(v - V))
 		if delta < theta:
 			break
-
