9840a00fc8dfdc11527f201cbac04a9c5bf26c66
oobx <robert.patryk.grzelak@gmail.com>
2024-02-26 23:16:43 +0100

    up

--- ReinforcementLearning/by book/jacks_rental.py
+++ ReinforcementLearning/by book/jacks_rental.py
@@ -16,22 +16,21 @@ class RequestsPerDay:
 	loc_2 = 0
 
 
-class ReturnsPerDay:
-	loc_1 = np.random.poisson(lam=3.0)
-	loc_2 = np.random.poisson(lam=2.0)
+# class ReturnsPerDay:
+# 	def __init__(self):
+# 		self.loc_1 = np.random.poisson(lam=3.0)
+# 		self.loc_2 = np.random.poisson(lam=2.0)
 
 
 class RentalBusiness:
 	def __init__(self):
 		self.day = 1
-		self.returned_at_1 = ReturnsPerDay.loc_1
-		self.returned_at_2 = ReturnsPerDay.loc_2
-		self.moved = 0
-		self.rent_at_1 = self.check_availability(Locations.loc_1, RequestsPerDay.loc_1)
-		self.rent_at_2 = self.check_availability(Locations.loc_2, RequestsPerDay.loc_2)
+		self.moved_1_2 = 0
+		self.moved_2_1 = 0
+		# self.rent_at_1 = self.check_availability(Locations.loc_1, RequestsPerDay.loc_1)
+		# self.rent_at_2 = self.check_availability(Locations.loc_2, RequestsPerDay.loc_2)
 		self.available: int = 0
-
-		print('rented_at_1: ', self.rent_at_1)
+		# ic(self.rent_at_2)
 		self.actions_def = {
 			# '1': self.move_cars('first', 'second', self.rent_at_2),
 			# '2': self.move_cars('second', 'first', self.rent_at_1)
@@ -43,24 +42,24 @@ class RentalBusiness:
 		return self.day
 
 	def check_availability(self, cars_at_location, cars_num):
-		if cars_at_location and self.day:  # available immediately
-			cars_num = cars_num if cars_num < cars_at_location else cars_at_location
-		if self.day > 1:  # available the next day
-			# self.returned_at_1_list.append(ReturnsPerDay.loc_1)
-			# self.returned.append(self.move_cars('second', 'first', self.rent_at_1))
-
-			print('returned + moved', self.returned_at_1, self.moved)
-			return cars_num + self.returned_at_1 + self.moved
+		# if cars_at_location and self.day:  # available immediately
+		ic(cars_num)
+		cars_num = cars_num if cars_num < cars_at_location else cars_at_location
+		if self.day >= 2:  # available the next day
+		# 	print('returned + moved', Locations.loc_1, self.moved_1_2)
+			return cars_num + self.moved_1_2
 		# self.timestep()
 		return cars_num
 
 	def requests(self):
-		Locations.loc_1 -= self.rent_at_1
-		Locations.loc_2 -= self.rent_at_2
+		# Locations.loc_1 -= np.random.poisson(lam=3.0)
+		Locations.loc_1 -= self.check_availability(Locations.loc_1, np.random.poisson(lam=3.0))
+		Locations.loc_2 -= self.check_availability(Locations.loc_2, np.random.poisson(lam=4.0))
 
 	def returns(self):
-		Locations.loc_1 += self.returned_at_1
-		Locations.loc_2 += self.returned_at_2
+		self.timestep()
+		Locations.loc_1 += np.random.poisson(lam=3.0)
+		Locations.loc_2 += np.random.poisson(lam=2.0)
 
 	def move_cars(self, from_: str, to_: str, quantity: int) -> int:
 		cost = 2
@@ -92,16 +91,13 @@ ic(Locations.loc_2)
 
 RB = RentalBusiness()
 
-# ic(RB.move_cars(from_='first', to_='second', quantity=8))
 
-RB.requests()
-RB.returns()
 
-print(RB.day)
+for _ in range(3):
+	RB.requests()
+	RB.returns()
+	ic(Locations.loc_1)
+
 ic()
 ic(Locations.loc_1)
 ic(Locations.loc_2)
-
-ic(RB.commission())
-ic(RB.total())
-print(RB.timestep())
