ccd2438a53f84403e40b8e6e4eb9426b0d3d2241
Robb <56496611+robb666@users.noreply.github.com>
2024-07-19 09:24:35 +0200

    up

--- ReinforcementLearning/lunar lander/LunarLander2020.py
+++ ReinforcementLearning/lunar lander/LunarLander2020.py
@@ -4,11 +4,15 @@ from simple_dqn_torch_2020 import Agent
 from util import plot_learning_curve
 import numpy as np
 from icecream import ic
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 print(gym.__version__)
+
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2', render_mode="human")
-    ic(env.observation_space.high)
+    # ic(env.action_space)
     load_checkpoint = True
     
     agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4,
@@ -17,19 +21,20 @@ if __name__ == '__main__':
     if load_checkpoint:
         agent.load_model()
     scores, eps_history = [], []
-    n_games = 15000  # 15000 for gymnasium
+    n_games = 15000  # 30000 used to be for gymnasium
     
     for i in range(n_games):
         score = 0
         done = False
         observation, info = env.reset()  # gymnasium
         # observation = env.reset()
-        ic(observation)
+        # ic(observation, info)
 
         while not done:
             # env.render()
             action = agent.choose_action(observation)
             observation_, reward, terminated, truncated, info = env.step(action)  # gymnasium
+            ic(reward)
             # observation_, reward, done, info = env.step(action)
             done = terminated or truncated  # gymnasium
 
@@ -43,7 +48,7 @@ if __name__ == '__main__':
 
         avg_score = np.mean(scores[-100:])
 
-        if i % 10 == 0 and i > 0:
+        if i % 100 == 0 and i > 0:
             print('episode ', i, 'score %.2f' % score,
                   'average score %.2f' % avg_score,
                   'epsilon %.2f' % agent.epsilon)
